c2d9dc7743ddf47cbc709e8dbb7ea461e3c6336a
Jens Kraemer <jk@jkraemer.net>
2018-05-18 08:55:59 +0800

    bugfix: use i18n unit name in issue view

--- app/helpers/supply_items_helper.rb
+++ app/helpers/supply_items_helper.rb
@@ -1,8 +1,12 @@
 module SupplyItemsHelper
 
+  def unit_name(item)
+    l :"label_supply_item_unit_#{item.unit}"
+  end
+
   def issue_supply_item_tag(issue_supply_item)
     item = issue_supply_item.supply_item
-    "#{item.name} (#{issue_supply_item.quantity} #{item.unit})"
+    "#{item.name} (#{issue_supply_item.quantity} #{unit_name item})"
   end
 
   def issue_supply_item_form_tags(issue_supply_items)
@@ -18,7 +22,7 @@ module SupplyItemsHelper
       tags = [
         text_field_tag('issue[issue_supply_items_attributes][][quantity]',
                        issue_supply_item.quantity, id: id, size: 3),
-        content_tag(:span, t(:"label_supply_item_unit_#{supply_item.unit}"), class: 'unit'),
+        content_tag(:span, unit_name(supply_item), class: 'unit'),
         content_tag(:label, supply_item.name, for: id),
         hidden_field_tag('issue[issue_supply_items_attributes][][supply_item_id]',  supply_item.id)
       ]
