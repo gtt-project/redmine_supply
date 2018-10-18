11b08881e130bc3e7614e4b150afd1b0a1f7deee
Jens Kraemer <jk@jkraemer.net>
2018-10-18 17:34:35 +0800

    do not render deletion link in overlay #19

--- app/helpers/supply_items_helper.rb
+++ app/helpers/supply_items_helper.rb
@@ -6,7 +6,7 @@ module SupplyItemsHelper
     } if issue_supply_items
   end
 
-  def issue_supply_item_form_tag(issue_supply_item, label: false)
+  def issue_supply_item_form_tag(issue_supply_item, label: false, deletable: false)
     supply_item = issue_supply_item.supply_item
     id = dom_id supply_item
     content_tag :p, id: "#{id}_wrap", class: 'issue_supply_item_wrap' do
@@ -15,9 +15,9 @@ module SupplyItemsHelper
         text_field_tag('issue[issue_supply_items_attributes][][quantity]',
                        issue_supply_item.quantity, id: id, size: 3),
         content_tag(:span, supply_item.unit_name, class: 'unit'),
-        link_to('', '#', class: 'icon icon-del'),
-        hidden_field_tag('issue[issue_supply_items_attributes][][supply_item_id]',  supply_item.id)
       ]
+      tags << link_to('', '#', class: 'icon icon-del') if deletable
+      tags << hidden_field_tag('issue[issue_supply_items_attributes][][supply_item_id]',  supply_item.id)
       unless issue_supply_item.new_record?
         tags << hidden_field_tag(
           'issue[issue_supply_items_attributes][][id]',
