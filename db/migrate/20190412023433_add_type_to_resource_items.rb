229da0a89cffff6ccb7a3b16d5e9a4c0a76ad258
Ko Nagase <nagase@georepublic.co.jp>
2022-05-16 15:52:00 +0900

    Fixed ActiveRecord::IrreversibleMigration issue when uninstalling plugin

--- db/migrate/20190412023433_add_type_to_resource_items.rb
+++ db/migrate/20190412023433_add_type_to_resource_items.rb
@@ -1,6 +1,11 @@
 class AddTypeToResourceItems < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :resource_items, :type, :string, null: false, default: 'Asset'
     change_column :resource_items, :type, :string, null: false, default: nil
+  end
+
+  def down
+    change_column :resource_items, :type, :string, null: false, default: 'Asset'
+    remove_column :resource_items, :type, :string, null: false, default: 'Asset'
   end
 end
